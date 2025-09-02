7179d3c4ed1e05578428db0b75131a9566bbbb59
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-09-02 16:31:40 +0200

    Added experimental decorators

--- src/decorators/experimental-decorators/decorators-01-starting-setup/src/app.ts
+++ src/decorators/experimental-decorators/decorators-01-starting-setup/src/app.ts
@@ -4,15 +4,29 @@ function Logger(constructor: Function) {
 }
 
 function LoggerFactory(logString: string) {
-    return function(constructor: Function) {
+    return function (constructor: Function) {
         console.log("Factory Logging...");
         console.log(logString);
         console.log(constructor);
     }
 }
 
+function WithTemplate(template: string, hookId: string) {
+    console.log('TEMPLATE FACTORY');
+    return function(constructor: any) {
+        console.log('Rendering template');
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector('h1')!.textContent = p.name;
+        }
+    }
+}
+
 @Logger
 @LoggerFactory('LOGGING - PERSON')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
     name = "Max";
 
