679257c83285a8db727f45ed4577425d9f23878c
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-09-02 04:50:13 +0200

    Added decorators

---
+++ src/decorators/ecmascript-decorators/decorator.ts
@@ -0,0 +1,60 @@
+function logger<T extends new (...args: any[]) => any>(
+    target: T,
+    ctx: ClassDecoratorContext
+) {
+    console.log('logger decorator');
+    console.log(target);
+    console.log(ctx);
+
+    return class extends target {//anonymous class
+        constructor(...args: any[]) {
+            super(...args);
+            console.log('class constructor');
+            console.log(this);
+        }
+    };
+}
+
+function autobind(
+    target: (...args: any[]) => any,
+    ctx: ClassMethodDecoratorContext
+) {
+    ctx.addInitializer(function (this: any) {
+        this[ctx.name] = this[ctx.name].bind(this);
+    });
+
+    return function (this: any) {
+        console.log('Executing original function');
+        target.apply(this);
+    };
+}
+
+function replacer<T>(initValue: T) {
+    return function replacerDecorator(
+        target: undefined,
+        ctx: ClassFieldDecoratorContext
+    ) {
+        console.log(target);
+        console.log(ctx);
+
+        return (initialValue: any) => {
+            console.log(initialValue);
+            return initValue;
+        };
+    };
+}
+
+@logger
+class Person1 {
+    @replacer('')
+    name = 'Max';
+
+    @autobind
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
+}
+
+const max = new Person1();
+const greet1 = max.greet;
+greet1();
