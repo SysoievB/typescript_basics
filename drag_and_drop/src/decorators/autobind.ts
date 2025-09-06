db9c2b8a392bd72e10980d79d2a04e5ebac5647c
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-09-06 08:30:27 +0200

    Added namespace imports

---
+++ drag_and_drop/src/decorators/autobind.ts
@@ -0,0 +1,14 @@
+namespace App {
+    // autobind decorator
+    export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+        const originalMethod = descriptor.value;
+        const adjDescriptor: PropertyDescriptor = {
+            configurable: true,
+            get() {
+                const boundFn = originalMethod.bind(this);//bind() - an object can borrow a method from another object
+                return boundFn;
+            }
+        };
+        return adjDescriptor;
+    }
+}
